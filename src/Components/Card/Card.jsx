c705926339a053ab44ec0bcda751db683d5d0a8e
Eugen <kondratsky2004@gmail.com>
2025-05-14 16:44:42 +0300

    next step

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -1,17 +1,45 @@
 import s from "../../Components/Card/Card.module.scss";
+import Check from "../../assets/Icons/check-fill.svg";
+import Button from "../Button/Button";
 
-function Card({ popular, description, highlight, smallText, direction ="column" }) {
+function Card({
+  popular,
+  title,
+  highlight,
+  direction = "column",
+  price,
+  per,
+  billing,
+  include,
+  purple,
+}) {
   return (
     <div className={`${s.card} ${direction === "row" ? s.row : s.column}`}>
-      <div className={popular ? s.MostPopular : undefined}>
-        <p>{popular}</p>
+      {popular && (
+        <div className={s.mostPopular}>
+          <p>{popular}</p>
+        </div>
+      )}
+      <div className={s.header}>
+        <p className={s.titleStyle}>{title}</p>
+        <p className={s.highlightStyle}>{highlight}</p>
       </div>
-      <p
-        className={smallText ? s.smallDescriptionStyle : s.descriptionStyle}
-      >
-        {description}
-      </p>
-      <p className={s.highlightStyle}>{highlight}</p>
+
+      <div className={s.priceGroup}>
+        <div className={purple ? s.purpleRow : s.blackRow}>
+          <span className={s.price}>{price}</span>
+          <span className={s.per}>{per}</span>
+        </div>
+        {billing && <p className={s.billing}>{billing}</p>}
+      </div>
+      <div className={s.include}>
+        {include.map((include, index) => (
+          <div key={index} className={s.includeItem}>
+            <img src={Check} alt="check icon" className={s.icon} />
+            <span>{include}</span>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
--- src/Components/Price/Price.jsx
+++ src/Components/Price/Price.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import s from "../../Components/Price/Price.module.scss";
 import CustomBlock from "../CustomBlock/CustomBlock";
 import Check from "../../assets/Icons/check-fill.svg";
@@ -5,6 +6,8 @@ import Button from "../Button/Button";
 import Card from "../Card/Card";
 
 function Price() {
+  const [isYearly, setIsYearly] = useState(false);
+
   return (
     <div className={s.price}>
       <div className={s.header}>
@@ -13,19 +16,84 @@ function Price() {
           description="Fit for all your needs"
           highlight="Pick the plan that suits you today and step up as your demands grow - our flexible options have your journey mapped out."
         />
+        <div className={s.toggleTabs}>
+          <div>
+            <button
+              className={`${s.tab} ${!isYearly ? s.active : ""}`}
+              onClick={() => setIsYearly(false)}
+            >
+              Monthly
+            </button>
+            <button
+              className={`${s.tab} ${isYearly ? s.active : ""}`}
+              onClick={() => setIsYearly(true)}
+            >
+              Annually
+            </button>
+          </div>
+        </div>
       </div>
+
       <div className={s.container}>
         <div className={s.card}>
-          <div className={s.headerCard}>
-        <Card
-          description="Basic Plan"
-          highlight="Access to a curated selection of abstract images"
-          smallText={true}
-        />
+          <Card
+            popular={false}
+            title="Basic Plan"
+            highlight="Access to a curated selection of abstract images"
+            price={isYearly ? "$99.99" : "$9.99"}
+            per={isYearly ? "/year" : "/month"}
+            billing={isYearly ? "Billed yearly" : "Billed monthly"}
+            include={[
+              "Standard quality images",
+              "Limited to personal use",
+              "Email support",
+            ]}
+          />
+          <Button type="secondary" label="Buy now" />
         </div>
+
+        <div className={s.card}>
+          <Card
+            purple={true}
+            popular={true}
+            title="Standard Plan"
+            highlight="Next-level Integrations, priced economically"
+            price={isYearly ? "$199.99" : "$19.99"}
+            per={isYearly ? "/year" : "/month"}
+            billing={isYearly ? "Billed yearly" : "Billed monthly"}
+            include={[
+              "Expanded library with more diverse abstract images",
+              "High-resolution images available",
+              "Suitable for commercial use",
+              "Priority email support",
+              "Advanced analytics",
+            ]}
+          />
+          <Button type="primary" label="Buy now" />
+        </div>
+
+        <div className={s.card}>
+          <Card
+            popular={false}
+            title="Premium Plan"
+            highlight="Experience limitless living for power users"
+            price={isYearly ? "$299.99" : "$29.99"}
+            per={isYearly ? "/year" : "/month"}
+            billing={isYearly ? "Billed yearly" : "Billed monthly"}
+            include={[
+              "Full access to the entire image library, including exclusive content",
+              "Highest quality images, including premium collections",
+              "Commercial and resale rights",
+              "Dedicated customer support line",
+              "24/7 support response time",
+              "Advanced analytics and insights",
+            ]}
+          />
+          <Button type="secondary" label="Buy now" />
         </div>
       </div>
     </div>
   );
 }
+
 export default Price;
